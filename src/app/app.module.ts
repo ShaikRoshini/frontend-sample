cbed15271737eb3f42afd71927891e838c5e5205
vipinreddym <vipin.m@mtwlabs.com>
2018-08-13 17:07:56 +0530

    intial commits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { SignupComponent } from './signup/signup.component';
+
+
+const routes: Routes = ([
+  {path:"",component:SignupComponent}
+])
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    FormsModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
